4ae966e42010d12536dbab3bb14ab750a6f81414
Mark Menard <mark@mjm.net>
2014-07-08 00:41:13 -0400

    Add String option support.

--- OptionParser/src/optionparser/OptionParser.java
+++ OptionParser/src/optionparser/OptionParser.java
@@ -4,23 +4,108 @@ import java.util.*;
 
 public class OptionParser {
 	
-	private final String optionsFromCommandLine;
-	private final Set<String> validFlags = new HashSet<String> ();
+	private final Map<String, CommandLineArgument> commandLineArgumentsByFlag = new HashMap<> ();
+	private final Map<String, String> configuredFlags = new HashMap<String, String> ();
+	private static final String STRING = "String";
+	private static final String BOOLEAN = "Boolean";
 	
-	public OptionParser (String optionsFromCommandLine, ParserConfig config) {
-		this.optionsFromCommandLine = optionsFromCommandLine;
+	public OptionParser (List<String> argsFromCommandLine, ParserConfig config) {
 		config.configure(this);
+		setupCommandLineArguments(argsFromCommandLine);
 	}
 
-	public Object getValue (String flag) {
-		return optionsFromCommandLine.contains("-" + flag);
-	}
-		
 	public void option (String flag) {
-		validFlags.add(flag);
+		option(flag, BOOLEAN);
 	}
 	
+	public void option (String flag, String type) {
+		configuredFlags.put(flag, type);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public Optional getValue (String flag) {
+		if (configuredFlags.get(flag) == STRING) {
+			return getValueForFlag(flag);
+		} else if (configuredFlags.get(flag) == BOOLEAN) {
+			return Optional.of(commandLineArgumentsByFlag.containsKey(flag));
+		}
+		return Optional.ofNullable(null);
+	}
+		
 	public Boolean hasConfiguredFlag (String flag) {
-		return validFlags.contains(flag);
+		return configuredFlags.keySet().contains(flag);
+	}
+	
+	public String getTypeForFlag (String flag) {
+		return configuredFlags.get(flag);
+	}
+	
+	public Boolean isValid () {
+		return commandLineArgumentsByFlag.values().stream()
+			.filter(arg -> arg.getType() == STRING)
+			.allMatch(arg -> arg.getRawValue() != null && arg.getRawValue().length() > 0);
+	}
+
+	private Optional<String> getValueForFlag (String flag) {
+		CommandLineArgument arg = commandLineArgumentsByFlag.get(flag);
+		
+		return arg != null ? Optional.of(arg.getRawValue()) : Optional.ofNullable(null);
+		
+	}
+	
+	private void setupCommandLineArguments(List<String> rawArgsFromCommandLine) {
+		List<CommandLineArgument> commandLineArguments = new ArrayList<> ();
+
+		rawArgsFromCommandLine.stream()
+			.filter(arg -> arg.length() >= 2)
+			.forEach((arg) -> {
+				String flag = arg.substring(1, 2);
+				String value = arg.length() > 2 ? arg.substring(2) : null;
+				commandLineArguments.add(new CommandLineArgument(flag, value));
+			});
+		
+		configuredFlags.keySet().forEach(flag -> {
+			commandLineArguments.stream()
+				.filter(cla -> cla.getFlag().equals(flag))
+				.findFirst()
+				.ifPresent(cla -> cla.setType(configuredFlags.get(flag)));
+		});
+		
+		commandLineArguments.forEach(cla -> commandLineArgumentsByFlag.put(cla.getFlag(), cla));
+	}
+	
+	public class CommandLineArgument {
+		private String flag;
+		private String rawValue;
+		private String type;
+		
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public CommandLineArgument (String flag, String rawValue) {
+			this.flag = flag;
+			this.rawValue = rawValue;
+		}
+
+		public String getFlag() {
+			return flag;
+		}
+		
+		public void setFlag(String flag) {
+			this.flag = flag;
+		}
+		
+		public String getRawValue() {
+			return rawValue;
+		}
+		
+		public void setRawValue(String rawValue) {
+			this.rawValue = rawValue;
+		}
 	}
 }
--- OptionParser/test/optionparser/OptionParserTest.java
+++ OptionParser/test/optionparser/OptionParserTest.java
@@ -2,48 +2,97 @@ package optionparser;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.junit.Test;
 
 public class OptionParserTest {
 
-	@Test
-	public void boolean_option_is_false_when_absent () {
-		OptionParser parser = new OptionParser ("", new ParserConfig () {
-			
-			@Override
-			public void configure (OptionParser parser) {
-				parser.option("v");
-			}
-		});
-		
-		assertFalse((Boolean) parser.getValue("v"));
-	}
+	private final List<String> emptyArgs = new ArrayList<> ();
 	
+	/**
+	 * This tests serves as an example of how to use the OptionParser.
+	 */
+	@SuppressWarnings("unchecked")
 	@Test
-	public void boolean_option_is_true_when_present () {
-		OptionParser parser = new OptionParser ("-v", new ParserConfig () {
-			
-			@Override
-			public void configure (OptionParser parser) {
-				parser.option("v");
-			}
+	public void multiple_options () {
+		OptionParser parser = new OptionParser (Arrays.asList("-v", "-ffoo", "-p"), p -> { 
+			p.option("f", "String");
+			p.option("v");
+			p.option("p");
+			p.option("s", "String");
 		});
 		
-		assertTrue((Boolean) parser.getValue("v"));
+		parser.getValue("s").ifPresent(arg -> fail("We shouldn't be able to get here because -s isn't in the ARGV collection."));
+		assertTrue((Boolean) parser.getValue("v").get());
+		assertTrue((Boolean) parser.getValue("p").get());
+		assertEquals("foo", (String) parser.getValue("f").get());
+		
 	}
 
+	@Test
+	public void returns_empty_optional_for_undefined_option () {
+		OptionParser parser = new OptionParser (Arrays.asList("-ffoo"), p -> p.option("v"));
+		assertFalse(parser.getValue("f").isPresent());
+	}
 	
 	@Test
 	public void can_define_a_boolean_option () {
-		OptionParser parser = new OptionParser ("", new ParserConfig () {
-			
-			@Override
-			public void configure (OptionParser parser) {
-				parser.option("v");
-			}
-		});
-		
+		OptionParser parser = new OptionParser (emptyArgs, p -> p.option("v"));
 		assertTrue(parser.hasConfiguredFlag("v"));
+		assertEquals("Boolean", parser.getTypeForFlag("v"));
 	}
+	
+	@Test
+	public void boolean_option_is_false_when_arg_is_absent () {
+		OptionParser parser = new OptionParser (emptyArgs, p -> p.option("v"));
+		assertEquals(false, parser.getValue("v").get());
+	}
+	
+	@Test
+	public void boolean_option_is_true_when_arg_is_present () {
+		OptionParser parser = new OptionParser (Arrays.asList("-v"), p -> p.option("v"));
+		assertEquals(true, parser.getValue("v").get());
+	}
+	
+	// String options
+	
+	@Test
+	public void can_define_a_string_option () {
+		OptionParser parser = new OptionParser (emptyArgs, p -> p.option("f", "String"));
+		assertTrue(parser.hasConfiguredFlag("f"));
+		assertEquals("String", parser.getTypeForFlag("f"));
+	}
+	
+	@Test
+	public void is_invalid_when_a_string_option_does_not_have_content () {
+		OptionParser parser = new OptionParser (Arrays.asList("-f"), p -> p.option("f", "String"));
+		assertFalse(parser.isValid());
+	}
+
+	@Test
+	public void is_valid_when_a_string_arg_has_content () {
+		OptionParser parser = new OptionParser (Arrays.asList("-ffoo"), p -> p.option("f", "String"));
+		assertTrue(parser.isValid());
+	}
+
+	@Test
+	public void is_valid_when_a_string_arg_is_not_present () {
+		OptionParser parser = new OptionParser (Arrays.asList(""), p -> p.option("f", "String"));
+		assertTrue(parser.isValid());
+	}
+
+	@Test
+	public void can_return_string_arg_value () {
+		OptionParser parser = new OptionParser (Arrays.asList("-ffoo"), p -> p.option("f", "String"));
+		assertEquals("foo", parser.getValue("f").get());
+	}
+	
+	@Test
+	public void get_value_for_absent_string_arg_returns_an_empty_optional () {
+		OptionParser parser = new OptionParser (emptyArgs, p -> p.option("f", "String"));
+		assertEquals(false, parser.getValue("f").isPresent());
+	}
+	
 
 }
